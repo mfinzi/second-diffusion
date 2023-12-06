bbb22211d2819b5ff97163ba2c44fdf295379dde
AndPotap <apotapczynski@gmail.com>
2023-12-06 18:51:15 -0500

    Tested the install scripts and made modifications

--- scripts/install_req_score.sh
+++ scripts/install_req_score.sh
@@ -1,6 +1,11 @@
+py -m pip install --upgrade pip
+py -m pip install jupyter
+py -m pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
 cd "$HOME/cola"
 py -m pip install .[dev]
+cd "$HOME/second-diffusion"
 py -m pip install tensorflow
+py -m pip install tensorflow_datasets
 py -m pip install tensorflow_gan
 py -m pip install scipy
 py -m pip install -U "jax[cuda12_pip]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
--- scripts/install_req_stable.sh
+++ scripts/install_req_stable.sh
@@ -1,3 +1,5 @@
+py -m pip install --upgrade pip
+py -m pip install jupyter
 py -m pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
 py -m pip install transformers
 py -m pip install --upgrade diffusers[torch]
